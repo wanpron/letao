7b4e72eb422c5e350cd71e2b860b5140f6143107
wanpron <986944399@qq.com>
2019-01-05 12:41:31 +0800

    完成公用的部分

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -9,3 +9,47 @@ $(document).ajaxStart(function() {
    
   })
   
+  // 二级菜单切换
+  $(function(){
+
+    $('.lt_aside .category').click(function(){
+
+      $(this).next().stop().slideToggle();
+    })
+
+    // 左边侧边栏切换功能
+    $('.icon_left').click(function(){
+      $('.lt_aside').toggleClass("hidemenu");
+      $('.lt_topbar').toggleClass("hidemenu");
+      $('.lt_main').toggleClass("hidemenu");
+
+    })
+
+      // 退出功能
+
+      $('.icon_right').click(function(){
+        $('#logoutModal').modal('show');
+
+      });
+
+      // 点击退出按钮,发送ajax请求,实现退出
+      $('#logoutBtn').click(function(){
+        $.ajax({
+
+          type:'get',
+          url:'/employee/employeeLogout',
+          datatype:'json',
+          success:function( info ){
+            console.log(info);
+            if(info.success) {
+             
+          location.href = "login.html";
+
+            }
+          }
+        })
+
+      })
+
+
+  })
